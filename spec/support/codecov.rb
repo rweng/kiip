cbcefc0785f6349a0919936b03247971943e469d
Robin Wenglewski <robin@wenglewski.de>
2015-12-18 20:14:30 +0100

    ignore spec folder for codecov

--- spec/support/codecov.rb
+++ spec/support/codecov.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+end
 if ENV['CI']=='true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
